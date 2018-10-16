f49ff2e4c784d0b378f16c9db0ba1fac57838360
Bermellet <36846245+Bermellet@users.noreply.github.com>
2018-10-16 15:43:36 +0200

    BodyParser interpreter

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 const questionRoutes = require('./api/questions');
 const answerRoutes = require('./api/answers');
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded( { extended:false }));
+
 app.use('/question', questionRoutes);
 app.use('/answer', answerRoutes);
 
