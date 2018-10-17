bacc05cf23602903dea099fe170265e54117f0e9
Joel <joel@tvrbo.pro>
2018-10-17 18:19:17 +0200

    remodeling

--- server.js
+++ server.js
@@ -3,6 +3,26 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
+async function startServer(){
+    try{
+        console.log("Connecting to database...");
+        connectDB();
+        console.log("Database connected...");
+        
+        console.log("Connecting to server...");
+        const server = http.createServer(app);
+        
+        server.listen(port);
+        console.log("Server connected...");
+    }
+    catch(err){
+        console.log(err);
+    }
+}
 
-server.listen(port);
+async function connectDB(){
+    //connectar client a bd i generar estructura inicial
+}
+
+//START SERVER + DB
+startServer();
