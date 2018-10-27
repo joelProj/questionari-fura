b25aa1649476c09662ebc985b872a3dc65091080
Albertka1 <97carreiraalbert@gmail.com>
2018-10-27 15:39:12 +0200

    BootStrap + CSS + Modal (unfinished)

--- URL_id.js
+++ URL_id.js
@@ -95,7 +95,7 @@ function generateOptionsButton(options){
     for (var i in options)
         HTML+= `
                 
-                <button type="button" id="button" class="btn btn-info" onclick="performPostRequest('${options[i]}'); return false;" >${options[i]}</button>
+                <button type="button" id="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModalCenter" onclick="performPostRequest('${options[i]}'); return false;" >${options[i]}</button>
                 
                 `
 }
@@ -118,6 +118,28 @@ function generateBody(id, text, options){
 function generateEndHTML(questionID){
     //console.log(questionID);
     HTML += ` 
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        ...
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
                 <br> 
                 <br> 
                 <br>
